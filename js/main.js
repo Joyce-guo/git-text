0c92aa2817b9fba471a81ee0c24b8fa7a904a7bb
Joyce <591703743@qq.com>
2017-06-19 10:33:07 +0800

    创建了项目的基本目录结构

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by px on 2017/4/27.
+ */
+
+// 页面加载完统一执行部分
+window.onload = function () {
+    showImg ();
+}
+// 功能：弹出图片
+function showImg () {
+    var imgList = document.getElementById(`img-list`);
+    document.getElementByTagName("img");
+    var showImg = document.getElementById(`showImg`);
+    for(var i = 0; i < imgList.length; i++) {
+          lll
+    }
+}
